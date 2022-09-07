4fc40c931458fe0e2c5290db11d9c1a1ab931270
patoviola <patricioviola26@gmail.com>
2022-09-06 23:09:21 -0300

    Interactuando con el DOM

--- js/main.js
+++ js/main.js
@@ -14,6 +14,15 @@ var direccionDeRetiro = prompt("Ingrese direccion de partida")
 const remitente1 = new Remitente(nombreRemitente, telefonoRemitente, direccionDeRetiro) 
 console.log(remitente1)
 
+let datosRemitente =document.createElement("div")
+datosRemitente.innerHTML = `<h4>Remitente:</h4>
+<ul>
+    <li>Nombre Remitente: ${nombreRemitente}</li>
+    <li>Contacto: ${telefonoRemitente}</li>
+    <li>Direccion de Retiro: ${direccionDeRetiro}</li>
+</ul>`
+document.body.appendChild(datosRemitente)
+
 //destinatario
 class Destinatario {
     constructor(nombre, telefono, direccion){
@@ -30,6 +39,14 @@ var direccionDeEntrega = prompt("Ingrese direccion de destino")
 const destinatario1 = new Destinatario(nombreDestinatario, telefonoDestinatario, direccionDeEntrega)
 console.log(destinatario1)
 
+let datosDestinatario =document.createElement("div")
+datosDestinatario.innerHTML = `<h4>Destinatario:</h4>
+<ul>
+    <li>Nombre Remitente: ${nombreDestinatario}</li>
+    <li>Contacto: ${telefonoDestinatario}</li>
+    <li>Direccion de Entrega: ${direccionDeEntrega}</li>
+</ul>`
+document.body.appendChild(datosDestinatario)
 
 // Seleccion de Paquete
 const cantidadDeKM = prompt("Indique cantidad de KM") // provisorio
@@ -41,19 +58,26 @@ const paqueteGrande = 100
 function calcularPaquetePequeño(cantidadDeKM, paquetePequeño){
     const pequeño = cantidadDeKM * paquetePequeño;
     console.log("El valor del viaje es: $" + pequeño)
-    alert("El valor del viaje es: $" + pequeño)
+    let valorFinal = document.createElement("div")
+    valorFinal.innerHTML = `<h3>Valor del Viaje: ${pequeño}</h3>`;
+    document.body.appendChild(valorFinal)
+
 }
 
 function calcularPaqueteMediano(cantidadDeKM, paqueteMediano){
     const mediano = cantidadDeKM * paqueteMediano;
     console.log("El valor del viaje es: $" + mediano)
-    alert("El valor del viaje es: $" + mediano)
+    let valorFinal = document.createElement("div")
+    valorFinal.innerHTML = `<h3>Valor del Viaje: $${mediano}</h3>`;
+    document.body.appendChild(valorFinal)
 }
 
 function calcularPaqueteGrande(cantidadDeKM, paqueteGrande){
     const grande = cantidadDeKM * paqueteGrande;
     console.log("El valor del viaje es: $" + grande)
-    alert("El valor del viaje es: $" + grande)
+    let valorFinal = document.createElement("div")
+    valorFinal.innerHTML = `<h3>Valor del Viaje: $${grande}</h3>`;
+    document.body.appendChild(valorFinal)
 }
 
 switch (tipoDePaquete) {
@@ -69,9 +93,3 @@ switch (tipoDePaquete) {
         default:
             alert("Error de paquete")
     }
-
-
-
-
-
-
